9d8e1ddfe4368d1d688fdc8b9a667766601ad81b
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2026-08-01 00:59:44 +0530

    initial setup

--- src/main/java/com/gunitha/grok/SpringAiApplication.java
+++ src/main/java/com/gunitha/grok/SpringAiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GrokApplication {
+public class SpringAiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GrokApplication.class, args);
+		SpringApplication.run(SpringAiApplication.class, args);
 	}
 
 }

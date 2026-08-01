82d24ce1c57aa1d644a23d5dc9776736e8410029
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2026-08-01 20:18:17 +0530

    initial setup

--- src/main/java/com/gunitha/springai/config/memory/MemoryChatClientConfig.java
+++ src/main/java/com/gunitha/springai/config/memory/MemoryChatClientConfig.java
@@ -1,4 +1,20 @@
 package com.gunitha.springai.config.memory;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MemoryChatClientConfig {
+
+    @Bean
+    public ChatClient grokMemoryChatClient(OpenAiChatModel openAiChatModel) {
+        return ChatClient.builder(openAiChatModel)
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .temperature(0.8))
+                .build();
+    }
+
 }
---
+++ src/main/java/com/gunitha/springai/controller/memory/MemoryChatClientController.java
@@ -0,0 +1,23 @@
+package com.gunitha.springai.controller.memory;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/memory/v1")
+public class MemoryChatClientController {
+
+    @Qualifier("grokMemoryChatClient")
+    @Autowired
+    ChatClient grokMemoryChatClient;
+
+    @GetMapping("chat")
+    public ResponseEntity<String> memory(String message) {
+        return ResponseEntity.ok(grokMemoryChatClient.prompt(message).call().content());
+    }
+}

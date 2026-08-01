907d3542eea6830aff245584307254b1b643b91c
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2026-08-01 14:57:20 +0530

    initial setup

--- src/main/java/com/gunitha/springai/config/ChatClientConfig.java
+++ src/main/java/com/gunitha/springai/config/ChatClientConfig.java
@@ -4,6 +4,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -29,6 +30,15 @@ public class ChatClientConfig {
     public ChatClient grokAdvisorChatClient(OpenAiChatModel openAiChatModel) {
         return ChatClient.builder(openAiChatModel)
                 .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    @Bean
+    public ChatClient grokOptionsChatClient(OpenAiChatModel openAiChatModel) {
+        return ChatClient.builder(openAiChatModel)
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .maxCompletionTokens(50)
+                        .temperature(0.8))
                 .build();
     }
 
---
+++ src/main/java/com/gunitha/springai/controller/GrokOptionsChatController.java
@@ -0,0 +1,28 @@
+
+package com.gunitha.springai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/options/v1")
+public class GrokOptionsChatController {
+
+    @Qualifier("grokOptionsChatClient")
+    @Autowired
+    ChatClient grokOptionsChatClient;
+
+    @GetMapping("chat")
+    public String chat(@RequestParam String message) {
+        return grokOptionsChatClient.prompt(message)
+                .options(OpenAiChatOptions.builder().model("llama-3.3-70b-versatile").maxCompletionTokens(100))
+                .system("You are AI assistant. You answer questions related to Spring boot Java implementation")
+                .call().content();
+    }
+}
